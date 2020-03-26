9012d8ed5d0c175f437f2803e2a61f6b15638f83
Kieran Glover <kieran@workwithmad.com>
2020-03-26 22:48:17 +0700

    :sparkles: Basic structure done

--- main.lua
+++ main.lua
@@ -1,14 +1,18 @@
 function love.load()
+    x  = 300
+    y  = 0
     ball = love.graphics.newImage("/assets/ball.png")
+    goal = love.graphics.newImage("/assets/goal.png")
     love.graphics.setBackgroundColor(0,0,0)
 end
 
-function love.update()
+function love.update(dt)
     -- Do math here
+    x = x + 2
+    -- y = y + (vy * dt)
 end
 
-
 function love.draw()
-    love.graphics.draw(ball)
-    love.graphics.print('Hello World!', 400, 300)
+    love.graphics.draw(ball, x, y)
+    love.graphics.draw(goal, 200, 520)
 end
