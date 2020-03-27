eaa745475257c2af8a170be9c567e02009ec53c9
Kieran Glover <kieran@workwithmad.com>
2020-03-27 20:22:12 +0700

    :sparkles: Basic movement

--- main.lua
+++ main.lua
@@ -1,18 +1,37 @@
+local world = love.physics.newWorld( 0, 9.81, true )
+
 function love.load()
-    x  = 300
-    y  = 0
+    love.graphics.setBackgroundColor(255,255,255)
+
+    player = {}
+    player.x  = 50
+    player.y  = 250
+    player.w = 50
+    player.h = 50
+
+    coin = {}
+    coin.x = 200
+    coin.y = 525
+    coin.w = 30
+    coin.h = 30
+
     ball = love.graphics.newImage("/assets/ball.png")
     goal = love.graphics.newImage("/assets/goal.png")
-    love.graphics.setBackgroundColor(0,0,0)
 end
 
 function love.update(dt)
-    -- Do math here
-    x = x + 2
-    -- y = y + (vy * dt)
+    if love.keyboard.isDown("right") then
+        player.x = player.x + 4
+    elseif love.keyboard.isDown("left") then
+        player.x = player.x - 4
+    elseif love.keyboard.isDown("up") then
+        player.y = player.y - 4
+    elseif love.keyboard.isDown("down") then
+        player.y = player.y + 4
+    end 
 end
 
 function love.draw()
-    love.graphics.draw(ball, x, y)
-    love.graphics.draw(goal, 200, 520)
+    love.graphics.draw(ball, player.x, player.y)
+    love.graphics.draw(goal, coin.x, coin.y)
 end
